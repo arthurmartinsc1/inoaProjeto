e77b0b38321051ad6a877f03482b97d8059b12ac
arthur_martins <arthur_donato@outlook.com>
2025-11-11 18:01:58 -0300

    Update binary files and assembly information for inoaProjectx

--- obj/Debug/net9.0/inoaProjectx.AssemblyInfo.cs
+++ obj/Debug/net9.0/inoaProjectx.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inoaProjectx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed4b0170e2071935e3f5e35dd9ced34d320c202")]
 [assembly: System.Reflection.AssemblyProductAttribute("inoaProjectx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inoaProjectx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
